8026466730086c174580f6066f4a2da1c9716bce
Jack <2784343633@qq.com>
2023-05-12 22:03:40 +0800

    new file:   LinkList/LinkList 	modified:   LinkList/LinkList.c 	modified:   LinkList/LinkList.h

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -11,10 +11,39 @@ LinkList List_HeadInsert(LinkList L)
     {
         s = (LNode *)malloc(sizeof(LNode));
         s->data = e;
-        /* 每次将s插入到位序为1的位置，存储顺序与输入顺序相反 */
+        /* 前插操作 */
         s->next = L->next; 
         L->next = s;
         scanf("%d", &e);
     }
     return L;
+}
+
+LinkList List_TailInsert(LinkList L)
+{   /* 正向建立单链表 */
+    L = (LinkList)malloc(sizeof(LNode));
+    L->next = NULL; // 此处非必需
+    LNode *s, *r=L;
+    ElemType e;
+    scanf("%d", &e);
+    while(e != 9999)
+    {
+        s = (LNode *)malloc(sizeof(LNode));
+        s->data = e;
+        // 后插操作
+        // s->next = r->next; 减少执行语句
+        r->next = s; 
+        r = s; // 更新尾结点指针
+        scanf("%d", &e);
+    }
+    r->next = NULL; // 尾结点指针置空
+    return L;
+}
+
+
+int main(void)
+{
+    LinkList L;
+    L = List_TailInsert(L);
+    return 0;
 }
--- LinkList/LinkList.h
+++ LinkList/LinkList.h
@@ -7,7 +7,8 @@ typedef struct LNode
 {
     /* data */
     ElemType data;
-    struct LinList *next;
+    struct LNode *next;
 } LNode, *LinkList; // LNode强调结点，LinkList强调链表本身
 
 LinkList List_HeadInsert(LinkList L);
+LinkList List_TailInsert(LinkList L);
